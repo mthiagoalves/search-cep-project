2777c2a5cb43c26b54290855f5edcceac764678a
mthiagoalves <m.thiago.alves@live.com>
2023-08-11 12:44:50 +0100

    chore(address): modify some table fields

--- app/Http/Repositories/CepRepository.php
+++ app/Http/Repositories/CepRepository.php
@@ -1,12 +1,63 @@
 <?php
 
-namespace App\Repositories;
+namespace App\Http\Repositories;
 
 use App\Models\Address;
+
+use Illuminate\Support\Facades\Validator;
 
 class CepRepository
 {
     public static function getAllCeps() {
         return Address::all();
     }
+
+    public static function getOneCep($cep) {
+        $findedCep = Address::where('cep', $cep)->get();
+
+        if ($findedCep->isEmpty()) {
+            $apiUrl = 'https://viacep.com.br/ws/' . $cep . '/json/';
+            $response = file_get_contents($apiUrl);
+
+            if ($response) {
+                $data = json_decode($response);
+
+                if (isset($data->erro) && $data->erro === true) {
+                    return ['message' => 'CEP não encontrado'];
+                } else {
+                    return $data;
+                }
+            } else {
+                return ['message' => 'CEP não encontrado'];
+            }
+        }
+
+        return $findedCep;
+    }
+
+    public static function createAddress($dataRequest){
+
+        $data = $dataRequest->all();
+
+        $dataAddress = Validator::make($data, [
+            "cep" => "required|integer",
+            "public_place" => "required|string",
+            "complement" => "nullable",
+            "burgh" => 'required|string',
+            "locality" => "required|string",
+            "state_acronym" => "required|string"
+        ]);
+
+        if ($dataAddress->fails()) {
+            $errors = $dataAddress->errors();
+
+            return ['errors' => $errors->all()];
+        }
+
+        $newAddress = Address::create($dataAddress->validated());
+
+        return $newAddress;
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'FrontendController@getAllCeps')->name('homepage');
 
+Route::get('/{cep}', 'FrontendController@getOneCep')->name('getOneCep');
+
+Route::post('/newAddress', 'FrontendController@createAddress')->name('createAddress');
+
